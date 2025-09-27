e11c89a91dacf28a03e158dcc719dc9b559e1a28
Aelita <aelita.lgbt@gmail.com>
2025-09-27 06:33:18 +0200

    Add the Dimok theme

--- site/js/index.js
+++ site/js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
 	// Enable Bootstrap tooltips
 	$('[data-toggle="tooltip"]').tooltip();
+
+	// Disable navigation hack
+	// TODO: Make this into a plugin so that it's done during the build instead
+	let tocSelector = $('disableToc');
+	if (tocSelector.length > 0) {
+		$('disableToc').remove();
+		$('div.col-md-3').remove();
+		$('div.col-md-9').attr('class', 'class-md-12');
+	}
 });
